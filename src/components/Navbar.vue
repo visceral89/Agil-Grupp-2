7225b3a210a61f22d4ebf33d6956c58b0ddfe948
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-02-17 15:35:57 +0100

    Router start & Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+	data() {
+		return {};
+	},
+};
+</script>
+<template>
+	<nav class="navbar-upper"></nav>
+	<nav class="navbar-lower"></nav>
+</template>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,13 @@
 // Router går här
+import { createRouter, createWebHistory } from "vue-router";
+
+export default createRouter({
+    history: createWebHistory("/"),
+    routes: [
+        {
+            path: "/",
+            name: "Login",
+            component:
+        }
+    ]
+})
