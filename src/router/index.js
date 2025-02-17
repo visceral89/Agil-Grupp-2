8f64b8097c1c6ebc1338ee4c99a3a747f546355d
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-02-17 15:42:05 +0100

    Router base done

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 export default createRouter({
-    history: createWebHistory("/"),
-    routes: [
-        {
-            path: "/",
-            name: "Login",
-            component:
-        }
-    ]
-})
+	history: createWebHistory("/"),
+	routes: [
+		{
+			path: "/",
+			name: "Login",
+			component: Hej,
+		},
+		{
+			path: "",
+			name: "Profil",
+			component: Hej,
+		},
+	],
+});
