352a887c80bcfe10cc80b7c4a04f15093df46fbc
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-18 07:33:11 +0100

    påbörjat startskärm och userinfo

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+
+}
+</script>
+
+<template>
+  <div class="user-wrapper">
+    <div class="user-img-wrapper">
+      <img src="" alt="Profilavtar">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
