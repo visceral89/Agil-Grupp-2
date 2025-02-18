c7e67ddae15b6c52517f0125c94fa8886a9aec97
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-02-18 09:16:46 +0100

    checkpoint charlie

--- src/components/NavbarTop.vue
+++ src/components/NavbarTop.vue
@@ -1,21 +1,33 @@
 <script>
 export default {
 	data() {
-		return { isExpanded: false };
+		return {};
 	},
 };
 </script>
 <template>
 	<nav class="navbar">
-		<span class="material-icons">account_circle</span>
-		<img src="../assets/FF-simple-logo.webp" alt="" />
-		<span class="material-icons">help</span>
-		<nav v-if="isExpanded"></nav>
+		<router-link to="/"> <span class="material-icons">account_circle</span></router-link>
+		<img src="../assets/FF-simple-logo.webp" alt="frage fejden logo" />
+		<router-link to="/"><span class="material-icons">help</span></router-link>
 	</nav>
 </template>
 <style scoped>
+span {
+	height: 40px;
+	width: 40px;
+}
+img {
+	width: 154px;
+	height: 77px;
+}
+
 .navbar {
+	width: 100%;
 	display: flex;
-	background: hsl(0, 0%, 60%);
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	background: transparent;
 }
 </style>
