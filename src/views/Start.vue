34f91ee697c11dbc6d6b5118da752532c751ae18
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-18 07:33:11 +0100

    påbörjat startskärm och userinfo

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,11 +1,25 @@
 <script>
 import { RouterLink } from 'vue-router';
+import UserInfo from '../components/UserInfo.vue';
+import UserInfo from '../components/UserInfo.vue';
 
-
+export default {
+  components: {
+    UserInfo
+  }
+}
 </script>
 
 <template>
-  <RouterLink></RouterLink>
+  <UserInfo />
+  
+  <RouterLink> <!-- btn to 1p game -->
+
+  </RouterLink>
+
+  <RouterLink> <!-- btn to 2p game -->
+
+  </RouterLink>
 </template>
 
 <style scoped>
