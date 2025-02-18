2b93fdfeecb24c4b46938987a4a748243af557ee
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-18 08:43:09 +0100

    start på profil

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -8,7 +8,10 @@ export default {
 <template>
   <div class="user-wrapper">
     <div class="user-img-wrapper">
-      <img src="" alt="Profilavtar">
+      <img src="../assets/images/906cd8d5-99ed-47cb-9f2b-ab4e18c8de1d.jpg" alt="Profilavtar">
+    </div>
+    <div id="welcome-text">
+      <p>Välkommen</p>
     </div>
   </div>
 </template>
