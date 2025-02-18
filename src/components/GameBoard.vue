307cae22d4ef2dc34b32dd9930889b2eee8997b2
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-18 16:53:25 +0100

    påbörjat style för gameboard

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="game-board">
     <div class="game-card">
-
+      <p>100p</p>
     </div>
   </div>
 </template>
@@ -16,5 +16,8 @@
 </script>
 
 <style scoped>
-
+  #gameboard {
+    border-radius: 6px;
+    background-color: rgba(246, 246, 246, 0.3);
+  }
 </style>
