50f1dc24cdf17293982a904b2b1d3566406368ff
Felicia Bengtsson <130352706+Tarawen@users.noreply.github.com>
2025-02-18 13:12:24 +0100

    start of layout

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="game-board">
+    <div class="game-card">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,13 +1,15 @@
 <script>
+  import GameBoard from '../components/GameBoard.vue';
 
-export default {
-  components: {
+  export default {
+    components: {
+      GameBoard
+    }
   }
-}
 </script>
 
 <template>
-
+  <GameBoard />
 </template>
 
 <style scoped>
