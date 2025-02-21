7d464e02c7690e6107f63ab4dfe9795e5bee6f77
patrikboyton <hello@patrikboyton.com>
2025-02-21 10:26:26 +0100

    Buggfix i knappkomponent

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :class="btn" @click="onClick">
+    <button @click="onClick">
         <slot>{{ label }}</slot>
     </button>
 </template>
@@ -21,12 +21,4 @@ export default {
 </script>
 
 <style scoped>
-.btn {
-    border-radius: 6px;
-    box-shadow: -2px 4px 0 0 rgba(0, 0, 0, 0.2);
-    padding: 1.375em 2.5625em;
-}
-.btn-game-card {
-    background-color: #E4CFF1; /* Ändra till variabel soft-pink */
-}
 </style>
