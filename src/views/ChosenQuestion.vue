ccaba3aca7df2ab4fb0eb9a694cc9eff83190b3f
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-21 12:27:36 +0100

    uppdaterat logik i chosenquestion så rätt fråga visas baserat på route params

--- src/views/ChosenQuestion.vue
+++ src/views/ChosenQuestion.vue
@@ -1,34 +1,25 @@
 <template>
   <div id="game-card-wrapper">
     <div class="question-wrapper">
-      <h2>Fråga</h2>
+      <h2>{{ selectedQuestion.question }}</h2>
     </div>
     
     <div id="question-card">
       <!-- chosen question from game board -->
-      <div class="answer-choice">
-        <span>1</span>
-      </div>
-      <div class="answer-choice">
-        <span>2</span>
-      </div>
-      <div class="answer-choice">
-        <span>3</span>
-      </div>
-      <div class="answer-choice">
-        <span>4</span>
+      <div v-for="(answer, index) in selectedQuestion.answers" :key="index" class="answer-choice">
+        <span>{{ answer }}</span>
       </div>
     </div>
 
     <!-- prepare for when props/import is done -->
-    <div v-if="selectedQuestion" id="question-card">
+<!--     <div v-if="selectedQuestion" id="question-card">
       <div class="question-wrapper">
         <h2>{{ selectedQuestion.question }}</h2>
       </div>
       <div  v-for="answer in selectedQuestion.answers" :key="selectedQuestion.id" class="answer-choice">
         <span>{{ answer }}</span>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -40,18 +31,18 @@ export default {
   data() {
     return {
       id: this.$route.params.id, //gets id from route param sent from gameboard
-      //if import of json-file
-      questions: questions,
+      questions: questions, //gets questions array from imported questions.json
+      selectedQuestion: null
       //if use of props
-      selectedQuestion: this.question
+/*       selectedQuestion: this.question */
     }
   },
-  props: {
+/*   props: {
     question: {
       type: Object,
       required: true
     }
-  },
+  }, */
   created() {
     this.getQuestion()
   },
@@ -59,8 +50,13 @@ export default {
     //fetches the corresponding question based on the id recieved from route-params
     getQuestion(id) {
       console.log(this.id, 'question id')
-      //om vi väljer att använda import av json och filter för att få fram frågorna.
-      /* this.selectedQuestion = this.questions.filter((question) => question.category === this.categoryList[1]) */
+      
+      //convert id to number
+      const questionId = parseInt(this.id)
+
+      //find the first correct element of the array questions
+      this.selectedQuestion = this.questions.find(question => question.id === questionId)
+      console.log(this.selectedQuestion, 'selectedQuestion')
     }
   }
 }
@@ -69,7 +65,7 @@ export default {
 <style scoped>
   .question-wrapper {
     width: 270px;
-    height: fit-content;
+    /* height: fit-content; */
     margin: auto;
   }
   h2 {
