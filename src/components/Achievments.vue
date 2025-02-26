0cacde5ab1941767cb2b7408bd153600d56fbbab
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-26 13:55:08 +0100

    grund till achievments

---
+++ src/components/Achievments.vue
@@ -0,0 +1,25 @@
+<template>
+   <div id="achievments-wrapper">
+      <div v-for="achievment in achievments">
+         {{ achievment }}
+      </div>
+   </div>
+</template>
+
+<script>
+   import { useUserStorage } from "../stores/storage";
+   import achievments from "../lib/achievments.json"
+
+   export default {
+      data() {
+         return {
+            userStorage: useUserStorage(), //gets userstorage data from storage.js
+
+         }
+  },
+   }
+</script>
+
+<style scoped>
+
+</style>
