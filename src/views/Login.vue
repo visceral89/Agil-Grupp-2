d642862de24609cc436ec08eecb3978a17b4a62a
patrikboyton <hello@patrikboyton.com>
2025-02-26 15:34:04 +0100

    Flyttat styles relaterade till Button-comp. Flyttat loginUser till storage.js

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -25,7 +25,7 @@ export default {
 			loginError: false,
 			loginPassword: '',
 			loginUsername: '',
-			users: users, //Överflödigt??
+			//users: users, //Överflödigt??
 			userStorage: useUserStorage() 
 		};
 	},
@@ -39,7 +39,7 @@ export default {
 
 			//Om värdet stämmer omdirigera till startskärmen, annars generera ett felmeddelande
 			if (loggedInUser) {
-				this.userStorage.activeUser = loggedInUser //Sparas loggedInUser till Pinia såhär? 
+				this.userStorage.loginUser(loggedInUser)
 				this.$router.push('/')
 			} else {
 				this.errorMessage = 'Fel inloggningsuppgifter.'
@@ -90,17 +90,5 @@ export default {
 	font-size: .8em;
 	position: absolute;
 	transform: translateY(7em);
-}
-
-Button {
-	background: var(--color-secondary);
-	padding: 9px 26px 9px 25px;
-	color: #ECECEC;
-	font-family: Poppins;
-	font-size: 20px;
-	font-weight: 500;
-	&:hover {
-		background-color: var(--color-card-hover);
-	}
 }
 </style>
