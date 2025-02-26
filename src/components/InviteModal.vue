f1828ebe5e9d4f65252ff97523fac6fbd414c881
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-02-26 13:45:56 +0100

    Jobbat på inbjudnings modalen

--- src/components/InviteModal.vue
+++ src/components/InviteModal.vue
@@ -1,25 +1,52 @@
 <template>
 	<div @click="$emit('is-invite-open')" id="modal-container">
 		<div id="flex-container">
-			<div id="user-list"></div>
+			<div id="user-list">
+				<div id="user-list-wrapper">
+					<h2>Lägg till motståndare</h2>
+					<div>
+						<div class="friend" v-for="user in users">
+							<img :src="user.avatar" alt="avatar" />
+							<p>{{ user.username }}</p>
+							<p id="points">Points: {{ user.points }}</p>
+							<span class="material-symbols-outlined"> add </span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 <script>
+import users from "../lib/users.json";
 export default {
 	data() {
-		return {};
+		return { users: users };
 	},
 };
 </script>
 <style scoped>
+h2,
+p,
+span {
+	color: var(--color-neutral-dark);
+}
+img {
+	/*
+	height: 24px;
+	width: 24px;
+   */
+}
+span {
+	font-size: 1.8rem;
+}
 #modal-container {
 	position: absolute;
 	top: 0;
 	right: 0;
 	left: 0;
 	bottom: 0;
-	background-color: hsla(0, 0%, 0%, 0.5);
+	background-color: hsla(0, 0%, 0%, 0.2);
 	backdrop-filter: blur(5px);
 }
 #flex-container {
@@ -30,9 +57,30 @@ export default {
 }
 
 #user-list {
-	background-color: hsl(0, 0%, 90%);
+	z-index: 999;
+	overflow-y: scroll;
+	background-color: var(--color-neutral-light);
 	width: 300px;
-	height: 300px;
+	height: 600px;
 	border-radius: 12px;
+}
+#user-list-wrapper {
+	padding: 1.5rem;
+}
+#points {
+	font-size: 0.7rem;
+}
+.friend {
+	display: grid;
+	grid-template-columns: auto 1fr auto auto;
+	align-items: center;
+	border-bottom: 1px solid hsla(0, 0%, 0%, 0.1);
+}
+
+@media (min-width: 700px) {
+	#user-list {
+		height: 450px;
+		width: 700px;
+	}
 }
 </style>
