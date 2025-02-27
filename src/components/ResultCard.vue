c4ed53658ad252546bd4baeab6fd9aac85a3a336
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-27 13:59:11 +0100

    lagt till komponent för resultatskärm

---
+++ src/components/ResultCard.vue
@@ -0,0 +1,24 @@
+<template>
+   <div id="result-container">
+      <div id="result-wrapper">
+         <div class="player-wrapper">{{ userStorage.activeUser.username }}</div>
+         <div class="player-wrapper">Player 2</div>
+      </div>
+   </div>
+</template>
+
+<script>
+   import { useUserStorage } from "../stores/storage";
+   
+   export default {
+      data() {
+         return {
+            userStorage: useUserStorage(), //gets userstorage data from storage.js
+         }
+      }
+   }
+</script>
+
+<style scoped>
+
+</style>
