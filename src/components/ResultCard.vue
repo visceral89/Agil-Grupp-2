967b2f2da3a1b3b363d2bba316f2796a2280a8d4
Felicia Bengtsson <bengtssonfelicia@gmail.com>
2025-02-28 16:24:29 +0100

    lagt till breakpoint på resultcard

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -1,60 +1,58 @@
 <template>
    <div id="result-container">
       <div id="result-wrapper">
-         
-         <!--if 2-players-->
-         <div id="2-player-wrapper" v-if="userStorage.player2 !== null">
-            <div class="user-img-wrapper">
-               <img :src="base_url + userStorage.activeUser.avatar" alt="Profilavtar">
-            </div>
-            <div class="result-list-wrapper">
-               <h2>Grattis, {{ userStorage.activeUser.username }} vann!</h2>
-               <div class="result-list">
-                  <p>Resultat</p>
-                  <div class="result-card">
-                     <p>{{ userStorage.activeUser.username }}</p>
-                     <p>{{ userStorage.activeUser.points }}</p>
-                  </div>
-                  <div class="result-card">
-                     <p>Spelare 2</p>
-                     <p>100p</p>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <!--if 2-players-->         
-         
          <!--if 1-players-->
-         <div id="1-player-wrapper" v-else>
+         <div id="1-player-wrapper">
             <div class="user-img-wrapper">
-               <img :src="base_url + userStorage.activeUser.avatar" alt="Profilavtar">
+               <img :src="base_url + userStorage.activeUser.avatar" alt="Profilavtar" />
             </div>
             <div class="result-list-wrapper">
                <h2>Snyggt spelat! Du samlade {{ userStorage.activeUser.points }} poäng.</h2>
                <div class="result-list">
                   <p>Highscoreplats</p>
                   <div class="result-card">
-                     <p>{{ userStorage.activeUser.username }}</p>
-                     <p>{{ userStorage.activeUser.points }}</p>
+                     <p>Spelare 1</p>
+                     <p>100p</p>
                   </div>
                </div>
             </div>
          </div>
          <!--if 1-players-->
+
+         <!--if 2-players-->
+
+<!--           <div id="2-player-wrapper">
+            <div class="user-img-wrapper">
+               <img :src="base_url + userStorage.activeUser.avatar" alt="Profilavtar">
+            </div>
+            <div class="result-list-wrapper">
+               <h2>Grattis, spelare vann!</h2>
+               <div class="result-list">
+                  <p>Resultat</p>
+                  <div class="result-card">
+                     <p>{{ userStorage.activeUser.username }}</p>
+                     <p>{{ userStorage.activeUser.points }}</p>
+                     <p>Spelare 2</p>
+                     <p>100p</p>
+                  </div>
+               </div>
+            </div>
+         </div> -->
+
+         <!--if 2-players-->
+
       </div>
    </div>
 </template>
 
 <script>
    import { useUserStorage } from "../stores/storage";
-   import users from "../lib/users.json";
    
    export default {
       data() {
          return {
             userStorage: useUserStorage(), //gets userstorage data from storage.js
             base_url: "src/",
-            users: users
          }
       }
    }
@@ -111,7 +109,6 @@
       background-color: var(--color-card-secondary);
       box-shadow: var(--box-shadow);
       padding: 0 8px;
-      margin-bottom: 15px;
    }
    .result-card > p {
       color: var(--color-neutral-dark);
@@ -120,18 +117,22 @@
    }
 
    @media (min-width: 890px) {
-    h2 {
-      font-size: 1.7rem;
-    }
-    #question-card {
-      grid-template-columns: 1fr 1fr;
-      gap: 30px 53px;
-    }
-    .answer-choice {
-      font-size: 1.3rem;
-    }
-    #result-msg {
-      font-size: 1.2rem;
-    }
+      img {
+         width: 160px;
+      }
+      .result-list > p {
+         font-size: 1.3rem;
+      }
+      .result-card {
+         width: 300px;
+         height: 45px;
+         border-radius: 6px;
+         background-color: var(--color-card-secondary);
+         box-shadow: var(--box-shadow);
+         padding: 0 8px;
+      }
+      .result-card > p {
+         font-size: 1rem;
+      }
   }
 </style>
