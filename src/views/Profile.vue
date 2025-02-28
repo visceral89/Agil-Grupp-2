599049ccb9fcda9f9004ce76fddc14297f3ca854
lire <ilirjana.babaj@iths.se>
2025-02-28 13:49:46 +0100

    uppdaterat profile,vänlista

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="profile-wrapper">
     <div class="profile-header">
-      <img class="profile-avatar" :src="base_url + user.avatar" alt="Profilbild" @error="useDefaultAvatar">
+      <div class="avatar-container">
+        <img v-if="isTopScorer" class="crown" src="../assets/images/highscore-crown.webp" alt="Krona"/>
+        <img class="profile-avatar" :src="base_url + user.avatar" alt="Profilbild" @error="useDefaultAvatar">
+      </div>
       <h2 class="profile-name">{{ user.username }}</h2>
       <Achievments />
       <p class="points">{{ user.points }} p</p>
-      <button @click="showFriends = !showFriends" class="friend-button">Vänner</button>
-      <ul v-if="showFriends" class="friends-list">
-        <li v-for="friend in users" :key="friend.id" class="friend-item">
-          <img :src="base_url + friend.avatar" alt="Avatar" class="friend-avatar"/>
-          <span class="friend-name">{{ friend.username }}</span>
-          <span class="friend-points">{{ friend.points }} p</span>
-        </li>
-      </ul>
+
+      <h3 class="friends-title">Vänner</h3>
+
+      <div class="friends-list-container">
+        <ul class="friends-list">
+          <li v-for="friend in filteredFriends" :key="friend.id" class="friend-item">
+            <img :src="base_url + friend.avatar" alt="Avatar" class="friend-avatar"/>
+            <span class="friend-name">{{ friend.username }}</span>
+            <span class="friend-points">{{ friend.points }} p</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -20,19 +27,27 @@
 <script>
 import users from "../lib/users.json";
 import { useUserStorage } from "../stores/storage";
-import Achievments from '../components/Achievments.vue'
+import Achievments from '../components/Achievments.vue';
 
 export default {
   data() {
     return {
-      base_url: "src/",userStorage:useUserStorage(),
-      showFriends: false,
-      users: users
+      base_url: "src/",
+      userStorage: useUserStorage()
     };
   },
   computed: {
     user() {
-      return useUserStorage().activeUser;
+      return this.userStorage.activeUser;
+    },
+    filteredFriends() {
+      return users.filter(friend => friend.id !== this.user.id);
+    },
+    maxPoints() {
+      return Math.max(...users.map(user => user.points));
+    },
+    isTopScorer() {
+      return this.user.points === this.maxPoints;
     }
   },
   methods: {
@@ -59,6 +74,20 @@ export default {
   text-align: center;
 }
 
+.avatar-container {
+  position: relative;
+  display: inline-block;
+}
+
+.crown {
+  position: absolute;
+  top: -55px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 80px;
+  height: auto;
+}
+
 .profile-avatar {
   width: 120px;
   height: 120px;
@@ -79,54 +108,55 @@ export default {
   font-weight: bold;
 }
 
-.friend-button {
-  background-color: var(--color-secondary);
-  color: white;
-  padding: 10px 15px;
-  border: none;
-  border-radius: var(--border-radius);
+.friends-title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
   margin-top: 15px;
-  cursor: pointer;
-  transition: var(--transition);
+  margin-bottom: 5px;
+  color: var(--color-neutral-light);
 }
 
-.friend-button:hover {
-  background-color: var(--color-card-hover);
+.friends-list-container {
+  width: 100%;
+  max-width: 600px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  margin-top: 0px;
 }
 
 .friends-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+  padding: 0;
   list-style: none;
-  margin-top: 10px;
-  background: var(--color-card-background);
-  border-radius: var(--border-radius);
-  box-shadow: var(--box-shadow);
-  padding: 10px;
-  width: 300px;
 }
 
 .friend-item {
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  padding: 10px;
-  font-size: 1.2rem;
-  color: var(--color-neutral-dark);
-  border-bottom: 1px solid hsla(0, 0%, 0%, 0.1);
+  text-align: center;
 }
 
 .friend-avatar {
-  width: 40px;
-  height: 40px;
+  width: 45px;
+  height: 45px;
   border-radius: 50%;
-  margin-right: 10px;
+  margin-bottom: 3px;
 }
 
 .friend-name {
-  flex-grow: 1;
+  font-size: 0.9rem;
+  font-weight: bold;
 }
 
 .friend-points {
-  font-weight: bold;
+  font-size: 0.8rem;
   color: var(--color-secondary);
 }
 </style>
+
