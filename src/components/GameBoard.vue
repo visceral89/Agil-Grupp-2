55341ec4501f579864e515ac7a21d23b3e91a045
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-03-03 14:50:16 +0100

    små fixar

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,155 +1,168 @@
 <template>
-	<div id="game-wrapper">
-		<div class="question-wrapper">
-			<h2>Välj fråga</h2>
-		</div>
+    <div id="game-wrapper">
+        <div class="question-wrapper">
+            <h2>Välj fråga</h2>
+        </div>
 
-		<div id="game-board">
-			<div class="category-column" v-for="(category, index) in categoryList" :key="index">
-				<div class="category-card">{{ category }}</div>
-				<router-link
-					v-for="(question, questionIndex) in questionsList[index]"
-					:key="questionIndex"
-					:to="`/game/${question.id}`"
-					style="display: contents">
-					<QuestionCard :question="question" />
-				</router-link>
-			</div>
-		</div>
-		<div id="player-container">
-			<div id="player">{{ userStorage.activeUser.username }}</div>
-			<div id="opponent" @click="toggleInviteModal">
-				{{ displayOpponent }}
-			</div>
-		</div>
-		<InviteModal v-if="isInviteOpen" @is-invite-open="toggleInviteModal" />
-	</div>
+        <div id="game-board">
+            <div
+                class="category-column"
+                v-for="(category, index) in categoryList"
+                :key="index"
+            >
+                <div class="category-card">{{ category }}</div>
+                <router-link
+                    v-for="(question, questionIndex) in questionsList[index]"
+                    :key="questionIndex"
+                    :to="`/game/${question.id}`"
+                    style="display: contents"
+                >
+                    <QuestionCard :question="question" />
+                </router-link>
+            </div>
+        </div>
+        <div id="player-container">
+            <div id="player">{{ userStorage.activeUser.username }}</div>
+            <div id="opponent" @click="toggleInviteModal">
+                {{ displayOpponent }}
+            </div>
+        </div>
+        <InviteModal v-if="isInviteOpen" @is-invite-open="toggleInviteModal" />
+    </div>
 </template>
 
 <script>
-import questions from "../lib/questions.json";
-import QuestionCard from "./QuestionCard.vue";
-import InviteModal from "./InviteModal.vue";
-import { useUserStorage } from "../stores/storage";
-export default {
-	data() {
-		return {
-			isInviteOpen: false,
-			questions: questions,
-			categoryList: [],
-			questionsList: [],
-			userStorage: useUserStorage(),
-		};
-	},
-	components: { QuestionCard, InviteModal },
-	methods: {
-		createBoard() {
-			//Hantera Kategorier
-			// Skapa ett set av en array, alla unika.
-			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+    import questions from "../lib/questions.json"
+    import QuestionCard from "./QuestionCard.vue"
+    import InviteModal from "./InviteModal.vue"
+    import { useUserStorage } from "../stores/storage"
+    export default {
+        data() {
+            return {
+                isInviteOpen: false,
+                questions: questions,
+                categoryList: [],
+                questionsList: [],
+                userStorage: useUserStorage()
+            }
+        },
+        components: { QuestionCard, InviteModal },
+        methods: {
+            createBoard() {
+                //Hantera Kategorier
+                // Skapa ett set av en array, alla unika.
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
 
-			this.categoryList = [...new Set(this.questions.map((question) => question.category))];
+                this.categoryList = [
+                    ...new Set(
+                        this.questions.map((question) => question.category)
+                    )
+                ]
 
-			for (let i = 0; i < this.categoryList.length; i++) {
-				this.questionsList.push(
-					this.questions.filter((question) => question.category === this.categoryList[i])
-				);
-			}
-			console.log(this.questionsList);
-		},
-		toggleInviteModal() {
-			this.isInviteOpen = !this.isInviteOpen;
-			console.log(this.isInviteOpen);
-		},
-	},
+                for (let i = 0; i < this.categoryList.length; i++) {
+                    this.questionsList.push(
+                        this.questions.filter(
+                            (question) =>
+                                question.category === this.categoryList[i]
+                        )
+                    )
+                }
+                console.log(this.questionsList)
+            },
+            toggleInviteModal() {
+                this.isInviteOpen = !this.isInviteOpen
+                console.log(this.isInviteOpen)
+            }
+        },
 
-	created() {
-		this.createBoard();
-		this.userStorage.setPlayers();
-	},
-	computed: {
-		displayOpponent() {
-			return this.userStorage.player2?.username || "Bjud in vän";
-		},
-	},
-};
+        created() {
+            this.createBoard()
+            this.userStorage.setPlayers()
+        },
+        computed: {
+            displayOpponent() {
+                return this.userStorage.player2?.username || "Bjud in vän"
+            }
+        }
+    }
 </script>
 
 <style scoped>
-h2 {
-	color: #fff;
-	text-align: center;
-	font-size: 32px;
-	font-style: normal;
-	font-weight: 700;
-}
-#game-wrapper {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-}
-#game-board {
-	display: grid;
-	grid-template-columns: repeat(3, auto);
-	gap: 10px;
-	grid-template-rows: auto;
-	align-items: center;
-	justify-items: center;
-	border-radius: 12px;
-	max-width: 1200px;
-	text-align: center;
-	background-color: rgba(246, 246, 246, 0.3);
-	margin: 0.5rem;
-	padding: 0.6rem 0.6rem;
-	padding-bottom: 0.9rem;
-}
-#player-container {
-	display: flex;
-	gap: 2rem;
-	margin-top: 2rem;
-	div {
-		display: flex;
-		padding: 1rem 2rem;
-		border-radius: var(--border-radius);
-		background-color: var(--color-secondary);
-	}
-}
+    h2 {
+        color: #fff;
+        text-align: center;
+        font-size: 32px;
+        font-style: normal;
+        font-weight: 700;
+    }
+    #game-wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    #game-board {
+        display: grid;
+        grid-template-columns: repeat(3, auto);
+        gap: 10px;
+        grid-template-rows: auto;
+        align-items: center;
+        justify-items: center;
+        border-radius: 12px;
+        max-width: 1200px;
+        text-align: center;
+        background-color: rgba(246, 246, 246, 0.3);
+        margin: 0.5rem;
+        padding: 0.6rem 0.6rem;
+        padding-bottom: 0.9rem;
+    }
+    #player-container {
+        display: flex;
+        gap: 2rem;
+        margin-top: 2rem;
+        cursor: pointer;
+        div {
+            display: flex;
+            padding: 1rem 2rem;
+            border-radius: var(--border-radius);
+            background-color: var(--color-secondary);
+        }
+    }
 
-.category-column {
-	display: flex;
-	flex-direction: column;
-	gap: 10px;
-}
+    .category-column {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
 
-.category-card {
-	background-color: var(--color-category-background);
-	text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
-	box-shadow: var(--box-shadow);
-	border-radius: var(--border-radius);
-	min-width: 82px;
-	min-height: 44px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	padding: 0.2rem 0.6rem;
-}
+    .category-card {
+        background-color: var(--color-category-background);
+        text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+        box-shadow: var(--box-shadow);
+        border-radius: var(--border-radius);
+        min-width: 82px;
+        min-height: 44px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0.2rem 0.6rem;
+    }
 
-@media (min-width: 500px) {
-	#game-board {
-		grid-column-gap: 10px;
-	}
-	.category-card {
-		min-width: 100px;
-	}
-	.category-column {
-		width: 100%;
-	}
-}
-@media (min-width: 700px) {
-	.category-card {
-		min-width: 200px;
-		padding: 0.2rem 0.6rem;
-	}
-}
+    @media (min-width: 500px) {
+        #game-board {
+            grid-column-gap: 10px;
+        }
+        .category-card {
+            min-width: 100px;
+        }
+        .category-column {
+            width: 100%;
+        }
+    }
+    @media (min-width: 700px) {
+        .category-card {
+            min-width: 200px;
+            padding: 0.2rem 0.6rem;
+        }
+    }
 </style>
--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,59 +1,70 @@
 <template>
-	<div @click="setDisabeled" class="question-card" :class="{ disabeled: isDisabeled }">
-		{{ question.points }}
-	</div>
+    <button
+        @click="setDisabeled"
+        class="question-card"
+        :disabled="isDisabeled"
+        :class="{ disabeled: isDisabeled }"
+    >
+        {{ question.points }}
+    </button>
 </template>
 <script>
-import { useQuestionStore } from "../stores/questionStore";
-export default {
-	props: ["question"],
-	data() {
-		return { isDisabeled: false, questionStore: useQuestionStore() };
-	},
-	created() {
-		console.log(this.question);
-		this.isDisabeled = this.questionStore.checkDisabeled(this.question.id);
-	},
-	methods: {
-		setDisabeled() {
-			this.questionStore.disableQuestion(this.question.id);
-		},
-	},
-};
+    import { useQuestionStore } from "../stores/questionStore"
+    export default {
+        props: ["question"],
+        data() {
+            return { isDisabeled: false, questionStore: useQuestionStore() }
+        },
+        created() {
+            console.log(this.question)
+            this.isDisabeled = this.questionStore.checkDisabeled(
+                this.question.id
+            )
+        },
+        methods: {
+            setDisabeled() {
+                this.questionStore.disableQuestion(this.question.id)
+            }
+        }
+    }
 </script>
 <style scoped>
-.question-card {
-	background-color: #e4cff1;
-	color: #181818;
-	box-shadow: -2px 4px 0 0 rgba(0, 0, 0, 0.2);
-	min-width: 82px;
-	min-height: 44px;
-	border-radius: var(--border-radius);
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	transition: var(--transition);
-}
-.question-card:hover {
-	background-color: #c99be5;
-	color: var(--color-neutral-light);
-}
+    .question-card {
+        background-color: #e4cff1;
+        color: #181818;
+        box-shadow: -2px 4px 0 0 rgba(0, 0, 0, 0.2);
+        min-width: 82px;
+        min-height: 44px;
+        border-radius: var(--border-radius);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: var(--transition);
+    }
+    .question-card:hover {
+        background-color: #c99be5;
+        color: var(--color-neutral-light);
+    }
 
-.disabeled {
-	background-color: #c99be5;
-	color: var(--color-neutral-light);
-	box-shadow: none;
-}
+    .disabeled {
+        background-color: var(--color-disabled);
+        color: var(--color-neutral-light);
+        box-shadow: none;
+        cursor: default;
+        &:hover {
+            background-color: var(--color-disabled);
+        }
+    }
 
-@media (min-width: 500px) {
-	.question-card {
-		min-width: 100px;
-	}
-}
-@media (min-width: 700px) {
-	.question-card {
-		min-width: 200px;
-		padding: 0.2rem 0.6rem;
-	}
-}
+    @media (min-width: 500px) {
+        .question-card {
+            min-width: 100px;
+        }
+    }
+    @media (min-width: 700px) {
+        .question-card {
+            min-width: 200px;
+            padding: 0.2rem 0.6rem;
+        }
+    }
 </style>
