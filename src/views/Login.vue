cdd8cc844611b658d8b34d0b1892609882d09534
patrikboyton <hello@patrikboyton.com>
2025-03-05 13:17:18 +0100

    Added option for guest login + styles and transition

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,22 @@
 <template>
 	<div class="login-wrapper">
 		<div class="logo-wrapper">
-			<img class="login-logo" src="../assets/logo/FF-logo.webp" alt="FrågeFejden logotyp">
+			<img class="login-logo" src="../assets/logo/FF-logo.webp" alt="FrågeFejden logotyp" />
 		</div>
-		<form @submit.prevent="handleLogin" class="login-form">
-			<input v-model="loginUsername" type="text" placeholder="Användarnamn">
-			<input v-model="loginPassword" type="password" placeholder="Lösenord">
-			<p v-if="loginError" class="login-error-msg">{{ errorMessage }}</p>
-			<Button>Logga in</Button>
-		</form>
+        <Transition name="fade" mode="out-in">
+            <form key="user" v-if="!guestForm" @submit.prevent="handleLogin" class="login-form">
+                <input v-model="loginUsername" type="text" placeholder="Användarnamn" />
+                <input v-model="loginPassword" type="password" placeholder="Lösenord" />
+                <p v-if="loginError" class="login-error-msg">{{ errorMessage }}</p>
+                <Button>Logga in</Button>
+                <p @click="toggleLogin" class="guest-login-p">Logga in som gäst</p>
+            </form>
+            <form key="guest" v-else @submit.prevent="handleGuestLogin" class="guest-login-form">
+                <input v-model="loginGuestname" type="text" placeholder="Gästnamn" />
+                <Button>Logga in</Button>
+                <p @click="toggleLogin" class="guest-login-p">Logga in som användare</p>
+            </form>
+        </Transition>
 	</div>
 </template>
 
@@ -24,19 +32,21 @@ export default {
 	data() {
 		return {
 			errorMessage: '',
+            guestForm: false,
 			loginError: false,
+            loginGuestname: '',
 			loginPassword: '',
 			loginUsername: '',
 			//users: users, //Överflödigt??
-			userStorage: useUserStorage() 
+			userStorage: useUserStorage()
 		};
 	},
 	methods: {
 		handleLogin() {
 			//Se ifall värdet i input-fälten stämmer överens med någon användare i users.json
 			const loggedInUser = users.find(
-				user => 
-				user.username === this.loginUsername && 
+				user =>
+				user.username === this.loginUsername &&
 				user.password === this.loginPassword)
 
                 console.log(loggedInUser)
@@ -48,7 +58,13 @@ export default {
 				this.errorMessage = 'Fel inloggningsuppgifter.'
 				this.loginError = true
 			}
-		}
+		},
+        handleGuestLogin() {
+            console.log("hej")
+        },
+        toggleLogin() {
+            this.guestForm = !this.guestForm
+        }
 	}
 };
 </script>
@@ -73,14 +89,15 @@ export default {
 	}
 }
 
-.login-form {
+.login-form,
+.guest-login-form {
 	align-items: center;
 	display: flex;
 	flex-direction: column;
 	margin-bottom: 4em;
 	input {
 		background: var(--color-neutral-light);
-		border: none;
+		border: 1px solid var(--color-neutral-dark);
 		border-radius: var(--border-radius);
 		display: flex;
 		justify-content: center;
@@ -95,9 +112,34 @@ export default {
 	}
 }
 
+.guest-login-p {
+    cursor: pointer;
+    font-size: .9em;
+    text-decoration: underline;
+    transition: var(--transition);
+    &:hover {
+        color: var(--color-accent);
+    }
+}
+
+.guest-login-form input {
+    margin: 2em 0 6em;
+}
+
 .login-error-msg {
 	font-size: .8em;
 	position: absolute;
 	transform: translateY(7em);
+}
+
+/* Form transition */
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 250ms ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
 }
 </style>
